1c9177d1f103aa0e9304af2ca945235478bda376
KrishnaKavya <m.krishnakavya@gmail.com>
2016-09-15 14:13:49 -0500

    Initial structure

---
+++ visual.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Word Trace</title>
+
+	<!--Jquery Library-->
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--stylesheet-->
+	<link rel="stylesheet" type="text/css" href="style/visualstyle.css">
+</head>
+<body>
+<div class="container-fluid">
+	<header>
+		<h1 class="title">Visual Annotation Blog</h1>
+	</header>
+
+	<p>
+	<b>Instructions</b>: 
+	 Highlight a word in the poem that interests you.  At the top of your post, type the phrase or clause that uses that word to help create an image.  Upload your visual and reflect briefly on the connection between your visual and the text.  
+	Also, do some research to create posts on the title and author as well as the time and place in which the poem was written.  You may add up to three media files per post.  
+	Browse posts of your peers and respond to some of them.  You might want to agree with their ideas and add some supportive thoughts or visuals in the comment box.  Or you might want to suggest a different approach.  You might even choose to redefine the image and work with a new concept.  Once an image has been annotated, the only way to add visuals or thoughts about that image is to comment on a post.  You may comment on any post and upload media files with your comments.
+
+	</p>
+	
+</div>
+</body>
