2eaf9b49ead349145b44dca71e319c6053cb13a1
KrishnaKavya <m.krishnakavya@gmail.com>
2016-09-20 12:07:17 -0500

    styling of the form and database connection

--- formpage.php
+++ formpage.php
@@ -18,7 +18,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!--CSS Style Sheet-->
-	
+	<link rel="stylesheet" type="text/css" href="style/formpagestyle.css">
 
 	<!--script file-->
 	
@@ -26,20 +26,37 @@
 <body>
 <!--Display uploaded comments-->
 <!--Form to upload comments-->
+<!--
+The form or the phase box is used as an input to upload new comments to the annotation blog. 
+The fields are 
+	-text of image or clause
+	-Add is inturn split into 3 categories
+		-uploading an image
+		-uploading a video
+		-adding the url of a web page.
+	- Reflection of the mediator. 
+The fields are indented according to the layout of the form. 
+
+
+-->
 <div class="container-fluid"></div>
-<form method="POST" enctype="multipart/form-data">
-Write text of image phrase or clause: <input type="text" name="text" size="40"><br>
+<div class="form">
+<form method="POST" enctype="multipart/form-data"> <br>
+Write text of image phrase or clause: <input type="text" name="text" size="40" class="inputfield"><br><br>
 Add: <br>
-<div>
+<div class="add">
 Image: <br>
 <input type="file" name="image" accept="image/*"/><br>
 Video:
-<input type="file" name="video" accept="image/*"/><br>
+<input type="file" name="video" accept="video/*"/ ><br>
 URL:
-<input type="text" name="url" accept="image/*"/><br>
+<input type="text" name="url" size="25" class="inputfield" ><br><br><br>
 </div>
+Reflect: <input type="text" name="text" size="40" class="inputfield"><br><br>
+<div class="submit">
 <input type="submit" name="submit">
-	
+</div>
+</div>	
 </form>
 </body>
 </html>
